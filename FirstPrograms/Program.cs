efffd8c7afe1be0134b54da2c99b111090bbc177
paninka123321 <56300478+paninka123321@users.noreply.github.com>
2025-07-20 19:56:24 +0200

    Add files via upload

---
+++ FirstPrograms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FirstPrograms
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program1. Enter the number. \n Please enter the positive number of your choice.");
+            EnterTheNumber.TakeNumber();
+
+            Console.WriteLine("Program2. Maximum Number");
+            MaximumNumber.FindMaximum();
+
+            Console.WriteLine("Program3. Type of Picture");
+            TypeOfPicture picture = new TypeOfPicture();
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            picture.x = width; 
+            picture.y = height; 
+            picture.IsLandscape();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
