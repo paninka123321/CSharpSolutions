ed89283ab5fe65b8b2d2ff8a0e1e5cb9625fa38b
paninka123321 <56300478+paninka123321@users.noreply.github.com>
2025-07-20 20:02:59 +0200

    classes

---
+++ Basics/Classes/Math/Calculatr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes.Math
+{
+    public class Calculatr
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public double Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return (double)a / b;
+        }
+    }
+}
---
+++ Basics/Classes/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace Classes
+{
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello, my name is {FirstName} {LastName}.");
+        }
+    }
+}
